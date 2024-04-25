53b28bf85a8c85e6c0f4c6839cefc4ac11d788e3
САБИР МАМУРОВ <gr423_masgha@lab-407-6.tomtit-tomsk.ru>
2024-04-25 09:33:41 +0700

    6_zadanie

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
 })
